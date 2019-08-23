a24040f468469392e55cb5134dd9972736c9fa96
waterlemon <429426523@qq.com>
2019-08-23 11:36:30 +0800

    (改正bug) 增加 iterator_category 的继承关系。

--- mystl_iterator.hpp
+++ mystl_iterator.hpp
@@ -23,9 +23,9 @@ namespace mystl
 /* classes */
 struct input_iterator_tag {};
 struct output_iterator_tag {};
-struct forward_iterator_tag {};
-struct bidirectional_iterator_tag {};
-struct random_access_iterator_tag {};
+struct forward_iterator_tag : public input_iterator_tag {};
+struct bidirectional_iterator_tag : public forward_iterator_tag {};
+struct random_access_iterator_tag : public bidirectional_iterator_tag {};
 
 template <typename Category, typename T,
          typename Distance = ptrdiff_t,
